5a1751e062afd838114cb1e493525dfa460d8fc7
JoaoTokarski <168202255+JoaoTokarski@users.noreply.github.com>
2025-05-30 11:26:35 -0300

    fff

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
+// Inicializa o valor do contador com 0
 let contador = 0;
 
+// Seleciona o botão e o elemento onde o número será exibido
 const botao = document.getElementById('botaoClique');
 const displayContador = document.getElementById('contador');
 
+// Adiciona um ouvinte de evento ao botão
+// Toda vez que o botão for clicado, o contador aumenta
 botao.addEventListener('click', () => {
-  contador++;
-  displayContador.textContent = contador;
+  contador++; // Incrementa o contador
+  displayContador.textContent = contador; // Atualiza o texto exibido
 });
-
