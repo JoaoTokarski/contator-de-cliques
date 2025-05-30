da25058d1d478e4e0662dc23034b36b50d34863c
JoaoTokarski <168202255+JoaoTokarski@users.noreply.github.com>
2025-05-30 11:14:51 -0300

    comeco fff

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+let contador = 0;
+
+const botao = document.getElementById('botaoClique');
+const displayContador = document.getElementById('contador');
+
+botao.addEventListener('click', () => {
+  contador++;
+  displayContador.textContent = contador;
+});
 
